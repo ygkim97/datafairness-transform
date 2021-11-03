dad45d5e4c94aceff4cacca3ec43c0ce0b5c0e67
ygkim97 <dnjdn21@naver.com>
2021-11-03 09:23:30 +0900

    feat : result 화면 UI수정 - result 화면 UI수정

--- vue-refine/src/views/layout/HeaderBar.vue
+++ vue-refine/src/views/layout/HeaderBar.vue
@@ -1,10 +1,7 @@
 <template>
   <v-app-bar color="blue-grey darken-3" app style="z-index: 17 !important;">
     <v-toolbar-title class="title left">
-      <span class="transition-swing text-h5 mb-1 white--text">
-        TABLE NAME :
-      </span>
-      <span class="transition-swing text-h4 mb-1 white--text">
+      <span class="transition-swing mb-1 white--text" style="font-size: 1.8rem">
         {{ tableName }}
       </span>
     </v-toolbar-title>
