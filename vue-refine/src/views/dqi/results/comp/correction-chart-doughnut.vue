3241921b032c4342a214421e7d09c981d0d3c07e
ygkim97 <dnjdn21@naver.com>
2021-12-27 10:48:03 +0900

    fix : prettier 적용 - prettier 적용

--- vue-refine/src/views/dqi/results/comp/correction-chart-doughnut.vue
+++ vue-refine/src/views/dqi/results/comp/correction-chart-doughnut.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <div class="d-flex justify-center py-4">
-      <h3>{{chartType.toUpperCase()}}</h3>
+      <h3>{{ chartType.toUpperCase() }}</h3>
     </div>
     <div class="d-flex justify-center">
       <div class="doughnut-wrap d-flex justify-center pt-10">
-        <canvas :id="'chart_doughnut_'+ chartType" width="200" height="300" class="d-flex"></canvas>
+        <canvas
+          :id="'chart_doughnut_' + chartType"
+          width="200"
+          height="300"
+          class="d-flex"
+        ></canvas>
         <div class="donut-inner">
           <span>{{ strTemplate[dataIndex] }}</span>
           <h3>{{ correctionData }}</h3>
@@ -20,106 +25,104 @@ import { Chart } from "chart.js";
 
 export default {
   props: ["chartType", "correctionData", "dataIndex", "afterCorrection"],
-    data() {
-      return{
-        chartObj: null,
-        datasets: [],
-        index: this.dataIndex,
+  data() {
+    return {
+      chartObj: null,
+      datasets: [],
+      index: this.dataIndex
+    };
+  },
+  methods: {
+    setLabels() {
+      let labels = [];
+      if (this.dataIndex === "missing_rate") {
+        labels = ["null", "not null"];
+      } else {
+        labels = [this.dataIndex, this.dataIndex.replaceAll("miss", "")];
       }
+      return labels;
     },
-    methods: {
-      setLabels() {
-        let labels = [];
-        if(this.dataIndex === "missing_rate"){
-          labels = ["null", "not null"];
-        } else {
-          labels = [this.dataIndex, this.dataIndex.replaceAll("miss", "")];
-        }
-        return labels;
-      },
-      createDatasets() {
-        this.datasets = [];
+    createDatasets() {
+      this.datasets = [];
 
-        let obj = {
-          data: [],
-          backgroundColor: [
-            "#F5A0A0",
-            "#577B91"
-          ]
-        };
-        const dataNum = Number(this.correctionData.replaceAll("%", ""));
-        obj.data.push(dataNum);
-        obj.data.push(100-dataNum);
+      let obj = {
+        data: [],
+        backgroundColor: ["#F5A0A0", "#577B91"]
+      };
+      const dataNum = Number(this.correctionData.replaceAll("%", ""));
+      obj.data.push(dataNum);
+      obj.data.push(100 - dataNum);
 
-        this.datasets.push(obj);
-      }
-      ,
-      createChart() {
-        let ctx = document.getElementById(`chart_doughnut_${this.chartType}`).getContext("2d");
+      this.datasets.push(obj);
+    },
+    createChart() {
+      let ctx = document
+        .getElementById(`chart_doughnut_${this.chartType}`)
+        .getContext("2d");
 
-        let config = {
-          type: "doughnut",
-          options: {
-            responsive: false,
-            plugins: {
-              legend: {
-                display: true,
-                position: "bottom"
-              },
-              title: {
-                display: false,
-                position: "bottom",
-                text: "Doughnut Chart"
-              },
-              datalabels: {
-                display: false,
-                backgroundColor: "white" ,
-                formatter: (value) => {
-                  return `${value}%`;
-                }
-              },
-              tooltip: {
-                callbacks: {
-                  label: function(tooltipItem) {
-                    return `${tooltipItem.label} : ${tooltipItem.raw}%`
-                  }
+      let config = {
+        type: "doughnut",
+        options: {
+          responsive: false,
+          plugins: {
+            legend: {
+              display: true,
+              position: "bottom"
+            },
+            title: {
+              display: false,
+              position: "bottom",
+              text: "Doughnut Chart"
+            },
+            datalabels: {
+              display: false,
+              backgroundColor: "white",
+              formatter: (value) => {
+                return `${value}%`;
+              }
+            },
+            tooltip: {
+              callbacks: {
+                label: function(tooltipItem) {
+                  return `${tooltipItem.label} : ${tooltipItem.raw}%`;
                 }
               }
             }
-          },
+          }
         }
+      };
 
-        config.data = {
-          labels: this.setLabels(),
-          datasets: this.datasets
-        };
+      config.data = {
+        labels: this.setLabels(),
+        datasets: this.datasets
+      };
 
-        this.chartObj = new Chart(ctx, config);
-      }     
-    },
-    mounted() {
-      const me =this;
-      this.$nextTick(() => {
-        me.createChart();
-      });
-    },
-    created() {
-      this.setLabels();
-    },
-    watch: {
-      afterCorrection() {
-        this.createDatasets();
-        this.chartObj.data.labels = this.setLabels();
-        this.chartObj.data.datasets = this.datasets;
-        this.chartObj.update();
-      }
-    },
-    computed: {
-      strTemplate() {
-        return this.$store.getters.CONSTANTS.result_str;
-      }
+      this.chartObj = new Chart(ctx, config);
     }
-}
+  },
+  mounted() {
+    const me = this;
+    this.$nextTick(() => {
+      me.createChart();
+    });
+  },
+  created() {
+    this.setLabels();
+  },
+  watch: {
+    afterCorrection() {
+      this.createDatasets();
+      this.chartObj.data.labels = this.setLabels();
+      this.chartObj.data.datasets = this.datasets;
+      this.chartObj.update();
+    }
+  },
+  computed: {
+    strTemplate() {
+      return this.$store.getters.CONSTANTS.result_str;
+    }
+  }
+};
 </script>
 
 <style scoped>
